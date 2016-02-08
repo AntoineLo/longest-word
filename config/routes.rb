7cbcda2b0bd095c6f2e0c718f530eb4548bc0145
Antoine Loron <loron.antoine@gmail.com>
2016-02-08 13:03:56 +0100

    Longest word on rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'game' => 'play#game', as: :game
+
+  get 'score' => 'play#score', as: :score
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
